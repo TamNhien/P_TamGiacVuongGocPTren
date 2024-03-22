fc5cb3ee9b187bcb2ced7b643e391c780c4bbf43
TamNhien <canhthien91@gmail.com>
2024-03-22 20:02:41 +0700

    Add project files.

---
+++ P_TamGiacVuongGocPTren.py
@@ -0,0 +1,10 @@
+﻿n = int(input("Nhập chiều cao tam giác: "))
+
+for i in range(n):
+    for j in range(n):
+        if j >= i:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+    print()
+
